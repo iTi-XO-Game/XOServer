22154bcaaf2b6506fe4b907a1ba161bee1155404
magdy <mohamedmagdy800k@gmail.com>
2026-01-01 17:36:54 +0200

    Create Class Message That has all the neede info between conection with serve and clint

--- src/main/java/com/tornado/xoserver/Message.java
+++ src/main/java/com/tornado/xoserver/Message.java
@@ -2,12 +2,36 @@ package com.tornado.xoserver;
 
 public class Message
 {
-    public String name;
-    public int age;
+//    public String name;
+//    public int age;
+//    //ssssssssssssssssssssssssss
+    public Header header;
+    public Object data;
 
-    public Message(String name, int age)
-    {
-        this.name = name;
-        this.age = age;
+    public Message(){
+        header = new Header();
+        data = new Object();
+    }
+
+    public Message(Header header, Object data){
+        this.header = header;
+        this.data = data;
+    }
+
+    // ===== Getters & Setters =====
+
+    public Header getHeader() {
+        return header;
+    }
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = (Object) data;
     }
 }
