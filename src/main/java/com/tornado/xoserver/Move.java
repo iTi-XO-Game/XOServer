badce2094b92555faf736660afb2db639597d2f5
magdy <mohamedmagdy800k@gmail.com>
2026-01-01 17:39:07 +0200

    Simler like LogIn Class:

--- src/main/java/com/tornado/xoserver/Move.java
+++ src/main/java/com/tornado/xoserver/Move.java
@@ -1,4 +1,32 @@
 package com.tornado.xoserver;
 
 public class Move {
+
+    private int row;
+    private int col;
+
+    public Move() {
+    }
+
+    public Move(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    // getters & setters
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
 }
