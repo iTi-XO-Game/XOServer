877551d7b62a54bdb777317d5de7509b542e1b58
magdy <mohamedmagdy800k@gmail.com>
2026-01-01 16:04:10 +0200

    Create Message class to handelt the connections betserver and the clints

---
+++ src/main/java/com/tornado/xoserver/Message.java
@@ -0,0 +1,15 @@
+package com.tornado.xoserver;
+
+import java.awt.*;
+
+public class Messge
+{
+    String name;
+    int age;
+
+    public  Messge(String naem, int age)
+    {
+        this.name = naem;
+        this.age = age;
+    }
+}
