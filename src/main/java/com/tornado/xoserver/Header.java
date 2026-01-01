dc95f7877e7b10e9d9eeae5353be3197d4fd8dde
magdy <mohamedmagdy800k@gmail.com>
2026-01-01 17:40:03 +0200

    Create Header class

--- src/main/java/com/tornado/xoserver/Header.java
+++ src/main/java/com/tornado/xoserver/Header.java
@@ -1,4 +1,56 @@
 package com.tornado.xoserver;
+/*
+should has
+1) enum has messageType
+2) enum has actionType
+*/
 
-public class Header {
+
+
+public class Header
+{
+    enum MessageType {
+        REQUEST,
+        RESPONSE,
+        ERROR,
+        EVENT
+    }
+
+    enum ActionType {
+        MOVE,
+        LOGIN
+    }
+
+
+    public MessageType messageType;
+    public ActionType actionType;
+    // now we have to create Constrctor and sitters , gitters
+
+
+    public Header() {
+
+    }
+
+    public Header(MessageType messageType, ActionType actionType) {
+        this.messageType = messageType;
+        this.actionType = actionType;
+    }
+
+    // ===== Getters & Setters =====
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(ActionType actionType) {
+        this.actionType = actionType;
+    }
+
 }
