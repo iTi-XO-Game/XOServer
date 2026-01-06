354cc2279b563a2afcc9c9784af242334c5d109a
Hossam Elgmmal <hossam.elgmmal12@gmail.com>
2026-01-06 08:47:56 +0200

    Wired the dashboard screen to the logic and improved the UI

---
+++ src/main/java/com/tornado/xoserver/Screen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tornado.xoserver;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Screen {
+    
+    public static final String DASHBOARD = "screens/dashboard";
+    public static final String USER_LIST_SCREEN = "screens/UserListScreen";
+}
--- src/main/java/com/tornado/xoserver/controllers/DashboardController.java
+++ src/main/java/com/tornado/xoserver/controllers/DashboardController.java
@@ -2,8 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.tornado.xoserver;
+package com.tornado.xoserver.controllers;
 
+import com.tornado.xoserver.App;
+import com.tornado.xoserver.Screen;
+import com.tornado.xoserver.server.ServerManager;
+import java.io.IOException;
 import javafx.fxml.*;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,12 +16,14 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 
 public class DashboardController implements Initializable {
@@ -35,44 +41,12 @@ public class DashboardController implements Initializable {
     private Label serverStatusLabel;
     @FXML
     private Button startServerButton, stopServerButton;
-    @FXML
-    private ImageView playIcon;
-    @FXML
-    private ImageView stopIcon;
-    @FXML
-    private ImageView usersIcon;
-
-    @FXML
-    private ImageView onlineIcon;
-
-    @FXML
-    private ImageView offlineIcon;
-
-    @FXML
-    private ImageView sessionsIcon;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        playIcon.setEffect(new ColorAdjust(0, 0, 1, 0));
-        stopIcon.setEffect(new ColorAdjust(0, 0, 1, 0));
-        makeRounded(usersIcon, 20);
-        makeRounded(onlineIcon, 20);
-        makeRounded(offlineIcon, 20);
-        makeRounded(sessionsIcon, 20);
         setupChart();
         loadLogs();
         setupStats();
-        setupActions();
-    }
-
-    private void makeRounded(ImageView imageView, double radius) {
-        Rectangle clip = new Rectangle(
-                imageView.getFitWidth(),
-                imageView.getFitHeight()
-        );
-        clip.setArcWidth(radius);
-        clip.setArcHeight(radius);
-        imageView.setClip(clip);
     }
 
     private void setupChart() {
@@ -104,31 +78,12 @@ public class DashboardController implements Initializable {
         onlineUsersLabel.setText("42");
         offlineUsersLabel.setText("1198");
         activeSessionsLabel.setText("8");
-
-        totalUsersCard.setOnMouseClicked(e
-                -> openUsers("Total Users", getAllUsers()));
-        onlineUsersCard.setOnMouseClicked(e
-                -> openUsers("Online Users", getOnlineUsers()));
-        offlineUsersCard.setOnMouseClicked(e
-                -> openUsers("Offline Users", getOfflineUsers()));
-    }
-
-    private void setupActions() {
-        startServerButton.setOnAction(e -> {
-            serverStatusLabel.setText("RUNNING");
-            logsList.getItems().add("INFO: Server started");
-        });
-
-        stopServerButton.setOnAction(e -> {
-            serverStatusLabel.setText("STOPPED");
-            logsList.getItems().add("INFO: Server stopped");
-        });
     }
 
     private void openUsers(String title, List<String> users) {
         try {
             FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("UserListScreen.fxml"));
+                    App.class.getResource(Screen.USER_LIST_SCREEN +".fxml"));
             Parent root = loader.load();
 
             UserListController controller = loader.getController();
@@ -139,8 +94,8 @@ public class DashboardController implements Initializable {
             stage.setTitle(title);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.show();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (IOException ex) {
+            //ex.printStackTrace();
         }
     }
 
@@ -163,6 +118,48 @@ public class DashboardController implements Initializable {
                 "GAME: Room created",
                 "WARN: Packet loss detected"
         );
+    }
+
+    @FXML
+    private void onStartClick(ActionEvent event) {
+        
+        ServerManager sManager = ServerManager.getInstance();
+        sManager.startServer(() -> {
+            Platform.runLater(() -> {
+                serverStatusLabel.setText("RUNNING");
+                logsList.getItems().add("INFO: Server started");
+            });
+        });
+    }
+
+    @FXML
+    private void onStopClick(ActionEvent event) {
+        ServerManager sManager = ServerManager.getInstance();
+        sManager.stopServer(() -> {
+            Platform.runLater(() -> {
+                serverStatusLabel.setText("STOPPED");
+                logsList.getItems().add("INFO: Server stopped");
+            });
+        });
+    }
+
+    @FXML
+    private void onTotalUsersClick(MouseEvent event) {
+        openUsers("Total Users", getAllUsers());
+    }
+
+    @FXML
+    private void onOnlineUsersClick(MouseEvent event) {
+        openUsers("Online Users", getOnlineUsers());
+    }
+
+    @FXML
+    private void onOfflineUsersClick(MouseEvent event) {
+        openUsers("Offline Users", getOfflineUsers());
+    }
+
+    @FXML
+    private void onActiveSessionsClick(MouseEvent event) {
     }
 
 }
