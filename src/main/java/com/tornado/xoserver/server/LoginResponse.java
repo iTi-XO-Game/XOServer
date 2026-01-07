0e69555e627948570ffa849a025b0d6c7542d622
Eldeep1 <alieldeep111@gmail.com>
2026-01-07 07:30:51 +0200

    setting up data transfer objects between client and server

---
+++ src/main/java/com/tornado/xoserver/server/LoginResponse.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tornado.xoserver.server;
+
+
+/**
+ *
+ * @author Depogramming
+ */
+
+public class LoginResponse {
+    private StatusCode statusCode;
+    private String errorMessage;
+    private Integer id;
+    private String userName;
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(StatusCode statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LoginResponse(StatusCode statusCode, String errorMessage) {
+        this.statusCode = statusCode;
+        this.errorMessage = errorMessage;
+    }
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(StatusCode statusCode, Integer id, String userName) {
+        this.statusCode = statusCode;
+        this.id = id;
+        this.userName = userName;
+    }
+
+}
