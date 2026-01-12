32164e72db5af2969313077db54d877b6500c0dd
magdy <mohamedmagdy800k@gmail.com>
2026-01-12 20:17:03 +0200

    Make it to fit with handling online players

--- src/main/java/com/tornado/xoserver/models/Stats.java
+++ src/main/java/com/tornado/xoserver/models/Stats.java
@@ -16,7 +16,11 @@ import javafx.beans.property.SimpleIntegerProperty;
 public class Stats {
     public static List<String> allPlayers = new ArrayList<String>();
     public static IntegerProperty total = new SimpleIntegerProperty();
-    public IntegerProperty online = new SimpleIntegerProperty();
+
+    public static List<String> allOnlinePlayers = new ArrayList<String>();
+    public static IntegerProperty online = new SimpleIntegerProperty();
+
+    public static List<String> allOfflinePlayers = new ArrayList<String>();
     public IntegerProperty offline = new SimpleIntegerProperty();
     public IntegerProperty sessions = new SimpleIntegerProperty();
 }
